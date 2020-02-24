cb59975afc5de803e6f5aff246bdd088bfd13c11
bohdan_kulchytskyi <merseneries@gmail.com>
2020-02-24 14:04:54 +0200

    Add func bubble_sort

--- basic/_local_package.py
+++ basic/_local_package.py
@@ -6,3 +6,16 @@ def get_digit_input(text):
         except ValueError:
             print("It must be digit.")
     return tmp
+
+
+def bubble_sort(array):
+    arr_len = len(array)
+
+    if arr_len == 0:
+        return 0
+
+    for j in range(arr_len):
+        for i in range(arr_len - 1):
+            if array[i] > array[i + 1]:
+                array[i], array[i + 1] = array[i + 1], array[i]
+    return array
