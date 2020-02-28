441fca10833aa16a660324a375dea8c8241f2f79
bohdan_kulchytskyi <merseneries@gmail.com>
2020-02-28 14:08:41 +0200

    Fix after testing

--- basic/email_slicer.py
+++ basic/email_slicer.py
@@ -1,3 +1,6 @@
+import re
+
+
 def split_email(emails):
     emails = [emails] if type(emails) == str else emails
     all_emails = []
@@ -13,11 +16,17 @@ def split_email(emails):
 def print_email(email):
     for dic in email:
         for key, value in dic.items():
-            print(f"{key}:{value}")
-        print("-"*40)
+            print(f"{key}: {value}")
+        print("-" * 40)
+
+
+def check_valid_email(email_text):
+    return bool(re.search(r"[a-zA-Z0-9]{4,}@([a-zA-Z0-9]{2,}\.){1,4}[a-zA-Z0-9]{2,3}", email_text))
 
 
 if __name__ == '__main__':
-    email_list = ["bondps@bonansatours.com", "baliprestigeho@dps.centrin.net.id", "witamgr@dps.centrin.net.id",
-                  "indahsuluh2002@yahoo.com.sg", "imz1991@yahoo.com"]
-    print_email(split_email(email_list))
+    input_email = input("Input email to split: ")
+    if check_valid_email(input_email):
+        print_email(split_email(input_email))
+    else:
+        print("Invalid email. Email can contain: a-z, A-Z, 0-9 and '.'\nExample: gronioman@gmail.com")
