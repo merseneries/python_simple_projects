53c2cfc272dc20fbe90439349789c67cd24ab472
bohdan_kulchytskyi <merseneries@gmail.com>
2020-02-28 16:30:32 +0200

    Fix after testing

--- basic/binary_search.py
+++ basic/binary_search.py
@@ -1,3 +1,4 @@
+import random
 from _local_package import bubble_sort
 
 
@@ -7,28 +8,25 @@ def binary_search(array, element, high, low=0):
         if element > middle search in right side, else left side.
         List must be sorted first.
     """
-
-    middle = low + (high - low) // 2
-    if middle >= low:
+    if high >= low:
+        middle = low + (high - low) // 2
         if element == array[middle]:
             return middle
         elif element > array[middle]:
-            return binary_search(array, element, middle + 1, high)
+            return binary_search(array, element, high, middle + 1)
         else:
-            return binary_search(array, element, 0, middle - 1)
+            return binary_search(array, element, middle - 1, low)
     else:
         return None
 
 
 if __name__ == "__main__":
-    import random
-
-    search_elem = 50
+    search_elem = 66
     list_int = [random.randint(-100, 100) for i in range(50)]
     for i, v in enumerate(list_int, 1):
         print(f"{v:4}", sep="  ", end="")
         if i % 10 == 0:
             print()
     length = len(list_int) - 1
-    result = binary_search(bubble_sort(list_int), search_elem, length)
+    result = binary_search(list_int, search_elem, length)
     print(f"Element = {search_elem}, index = {result}")
