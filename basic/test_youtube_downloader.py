dadf5928338a047d2e368cfee75bf6a6fea424a2
bohdan_kulchytskyi <merseneries@gmail.com>
2020-03-12 17:10:10 +0200

    Add validation tests for youtube_downloader

--- basic/test_youtube_downloader.py
+++ basic/test_youtube_downloader.py
@@ -2,23 +2,43 @@ import pytest
 from youtube_downloader import YouTubeDownloader
 
 
-# Video with 2K resolution
+# Video with 4K resolution, 60fps
 @pytest.fixture
-def high_you_tube():
+def high_youtube():
     return YouTubeDownloader("https://www.youtube.com/watch?v=LXb3EKWsInQ")
 
 
-# Video with 240p resolution
+# Video with 480p resolution, 30fps
 @pytest.fixture
-def low_you_tube():
-    return YouTubeDownloader("https://www.youtube.com/watch?v=1w4zhAtBl3k")
+def low_youtube():
+    return YouTubeDownloader("https://www.youtube.com/watch?v=HqAblLqkhyc")
 
 
-def test_methods_return(high_you_tube):
-    assert high_you_tube.get_video_adaptive() is not None
-    assert high_you_tube.get_all_streams() is not None
-    assert high_you_tube.get_audio() is not None
+def test_methods_return(high_youtube):
+    assert high_youtube.get_video_adaptive() is not None
+    assert high_youtube.get_all_streams() is not None
+    assert high_youtube.get_audio() is not None
 
 
-def test_get_stream_low(low_you_tube):
-    assert low_you_tube.get_stream("240p", audio_type="mp4") is not None
+@pytest.mark.parametrize(
+    "video_params", [("", "", ""), ("123", "456", "789"), ("\w\w", "\w\w", "\w\w"), (".*.*", ".*.*", ".*.*"),
+                     ("720", "mp4", "60fps"), ("4320p", "mp4", "60fps"), ("1080p", "web", "30fps"),
+                     ("480p", "webm", "30")])
+def test_get_itag_video_invalid(high_youtube, video_params):
+    assert high_youtube.get_itag_video(*video_params) is None, "Invalid values accepted"
+
+
+@pytest.mark.parametrize(
+    "video_params_high, video_params_low", [(("720p", "mp4", "30fps"), ("144p", "mp4", "30fps")),
+                                            (("1080p", "mp4", "60fps"), ("360p", "webm", "30fps")),
+                                            (("2160p", "webm", "60fps"), ("480p", "mp4", "30fps"))])
+def test_get_itag_video_valid(high_youtube, low_youtube, video_params_high, video_params_low):
+    assert high_youtube.get_itag_video(*video_params_high) is not None, "Valid values not accepted"
+    assert low_youtube.get_itag_video(*video_params_low) is not None, "Valid values not accepted"
+
+
+@pytest.mark.parametrize(
+    "video_params", [("240p", "mp4", "30fps", "mp4"), ("480p", "webm")]
+)
+def test_get_stream_low(low_youtube, video_params):
+    assert low_youtube.get_stream(*video_params) is not (None, None), "Invalid value accepted"
